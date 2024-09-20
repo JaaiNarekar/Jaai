6c24f266414a2fa309d627929260321787a1746c
Jaai Nerkar <jaainerkar12@gmail.com>
2024-09-20 12:18:19 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,31 @@
+package Factorial;
+
+import java.util.Scanner;
+
+public class Number {
+
+	public static void main(String[] args) {
+		Scanner S= new Scanner(System.in);
+		System.out.println("Enter any Number");
+		int num1= S.nextInt();
+		int Fact1=28*1;
+		int Fact2=13*1;
+	
+		for(int i=1;i=n-0;i++) {
+			System.out.println("Fact1");
+		} 
+		{
+			System.out.println("Fact2");
+		}
+	}
+}
+
+			
+		
+		
+		
+		
+
+	
+
+
---
+++ test1.java
@@ -0,0 +1,43 @@
+package Bank;
+
+import java.util.Scanner;
+
+public class test1{
+
+	public static void main(String args[]) {
+		public class bankdetails {
+		
+		Scanner s =new Scanner(System.in);{
+		System.out.println("Enter any bankdetails");
+		}
+		public void  deposite(){
+		System.out.println("Deposite");
+		}
+		public void withdraw (){
+			System.out.println("withdraw");
+			
+		}
+		public void checkbalance() {
+			System.out.println("checkbalance");
+		}
+		public void getDetails(b1.bankdetails) {
+		System.out.println(bank.deposite);
+		System.out.println(bank.withdraw);
+		System.out.println(bank.checkbalance);
+		}
+		}
+		}
+	
+		
+		
+		
+		
+	
+	
+		
+		 
+			
+		
+	
+
+
